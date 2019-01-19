43b88c14b3e4313ec05f030b110d602a60767c97
Steve Konves <stephen.konves@gmail.com>
2019-01-19 16:16:26 -0700

    feat: parse package name and version

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,45 @@
 import { ProxyHandler, APIGatewayProxyResult } from 'aws-lambda';
+import * as axios from 'axios';
+
 import * as buildInfo from './build-info.json';
+import { createError, ErrorCode, PackageVersion, Status } from './types.js';
 
 export const handler: ProxyHandler = async event => {
   // TODO implement
-  const response: APIGatewayProxyResult = {
-    headers: { 'x-build-tag': buildInfo.tag },
-    statusCode: 200,
-    body: JSON.stringify('Hello from Lambda and Travis!'),
-  };
-  return response;
+  try {
+    const { name, version } = parsePackageParam(
+      event.pathParameters.packageVersion,
+    );
+
+    const result: PackageVersion = {
+      name,
+      version,
+      algo: 'none',
+      status: Status.unknown,
+    };
+
+    const response: APIGatewayProxyResult = {
+      headers: { 'x-build-tag': buildInfo.tag },
+      statusCode: 200,
+      body: JSON.stringify(result),
+    };
+    return response;
+  } catch (err) {
+    return <APIGatewayProxyResult>{
+      headers: { 'x-build-tag': buildInfo.tag },
+      statusCode: 500,
+      body: JSON.stringify(createError(ErrorCode.INTERNAL_ERROR, err.message)),
+    };
+  }
 };
+
+function parsePackageParam(
+  packageVersion: string,
+): { name: string; version: string } {
+  const [name, version] = packageVersion.split('@');
+
+  return {
+    name,
+    version: version || 'latest',
+  };
+}
---
+++ src/types.ts
@@ -0,0 +1,61 @@
+import * as uuid from 'uuid';
+
+export type JsonApiError = {
+  id: string;
+  status: string;
+  code: ErrorCode;
+  title: string;
+  detail: string;
+  source?: {
+    pointer?: string;
+    parameter?: string;
+  };
+  meta?: any;
+};
+
+export type PackageVersion = {
+  name: string;
+  version: string;
+  algo: string;
+  status: Status;
+};
+
+export enum Status {
+  'unknown' = 'unknown',
+  'pending' = 'pending',
+  'verified' = 'verified',
+  'unverified' = 'unverified',
+}
+
+export function createError(code: ErrorCode, detail: string): JsonApiError {
+  return {
+    id: uuid.v4(),
+    status: errorTypes[code].status,
+    code,
+    title: errorTypes[code].title,
+    detail,
+  };
+}
+
+export enum ErrorCode {
+  NOT_FOUND = 'NOT_FOUND',
+  BAD_REQUEST = 'BAD_REQUEST',
+  INTERNAL_ERROR = 'INTERNAL_ERROR',
+}
+
+const errorTypes: {
+  [code: string]: { status: string; title: string };
+} = {
+  [ErrorCode.NOT_FOUND]: {
+    status: '404',
+    title: 'Resource not found for the current user',
+  },
+  [ErrorCode.BAD_REQUEST]: {
+    status: '400',
+    title: 'The request was invalid',
+  },
+  [ErrorCode.INTERNAL_ERROR]: {
+    status: '500',
+    title: 'Internal service error',
+  },
+};
