b423077c5b1abdd815c15b39ec76ea6f25c5b8ec
Steve Konves <stephen.konves@gmail.com>
2019-01-19 14:43:18 -0700

    chore: add build tag

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import { ProxyHandler } from 'aws-lambda';
+import { ProxyHandler, APIGatewayProxyResult } from 'aws-lambda';
+import * as buildInfo from './build-info.json';
 
 export const handler: ProxyHandler = async event => {
   // TODO implement
-  const response = {
+  const response: APIGatewayProxyResult = {
+    headers: { 'x-build-tag': buildInfo.tag },
     statusCode: 200,
     body: JSON.stringify('Hello from Lambda and Travis!'),
   };
