1866a4d8068611379ac40b163920cbb46d44fae3
Steve Konves <stephen.konves@gmail.com>
2019-01-19 13:41:49 -0700

    feat: change message

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ export const handler: ProxyHandler = async event => {
   // TODO implement
   const response = {
     statusCode: 200,
-    body: JSON.stringify('Hello from Lambda!'),
+    body: JSON.stringify('Hello from Lambda and Travis!'),
   };
   return response;
 };
